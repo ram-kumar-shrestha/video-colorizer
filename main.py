d2a87b2480cc29b960ea2298fba7587b6286e701
Ram Kumar Shrestha <34183131+ram-kumar-shrestha@users.noreply.github.com>
2022-02-17 18:49:24 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from colorizer import *
 
 colorizer = Colorizer(width = 640, height=480)
 
-colorizer.processImage("./video-colorizer/input/rose.jpg")
+# colorizer.processImage("./video-colorizer/input/rose.jpg")
 
-# colorizer.processVideo('./video-colorizer/input/old.mp4')
+colorizer.processVideo('./video-colorizer/input/old.mp4')
